64f72648b1f993a7258032cfd98001828f792395
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2020-04-18 16:29:29 +0100

    base

---
+++ src/Exceptional.jl
@@ -0,0 +1,11 @@
+block(func)=
+
+return_from(name, value=nothing)=
+
+available_restarts(name, args...)=
+
+restart_bind(func, restarts...)=
+
+error(exception::Exception)=
+
+handler_bind(func, handlers...)=
