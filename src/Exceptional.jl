ef0cd0e52948944cd5412f192240770433e37b61
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2020-05-01 22:23:52 +0100

    handler_bind

--- src/Exceptional.jl
+++ src/Exceptional.jl
@@ -1,5 +1,7 @@
 cnt = 0
 
+res = Pair[]
+
 struct BlockException <: Exception
   lbl 
   val
@@ -11,10 +13,7 @@ function block(func::Function)
   try
     func(lbl)
   catch e
-    if !isa(e, BlockException)
-      rethrow()
-    end
-    if e.lbl == lbl
+    if (e isa BlockException) && (e.lbl == lbl)
       return e.val
     else
       rethrow()
@@ -26,18 +25,37 @@ function return_from(lbl::String, val=nothing)
   throw(BlockException(lbl, val))
 end
 
-function available_restarts(name, args...)
-  # do something end
+function available_restart(name)
+  # TODO: Check of restart is available
+end
+
+function invoke_restart(name, args...)
+  # TODO: execute restart with the args provided
 end
 
 function restart_bind(func, restarts...)
-  # do something
+  # TODO: Make restarts available
+  func
 end
 
 function error(exception::Exception)
-  # do something
+  # XXX: What should this function do? other than throw an except?
+  throw(exception)
 end
 
-function handler_bind(func, handlers...)
-  # do something
+function handler_bind(func::Function, handlers...)
+  try
+    func()
+  catch e
+    for pair in handlers
+      e_type = pair.first
+      handle = pair.second 
+      if e isa e_type
+        handle(e_type)
+        break
+      end
+    end
+    # always rethrow
+    rethrow()
+  end
 end
